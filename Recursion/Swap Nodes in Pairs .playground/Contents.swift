2de81ceea484786f76fe2a40ce5b4fd06a6bf8f4
Ajinkya Shelar <ajinkya.shelar@gmail.com>
2020-02-24 19:47:05 +0530

    Swap Nodes in Pairs
    Swap Nodes in Pairs Solution Added

---
+++ Recursion/Swap Nodes in Pairs .playground/Contents.swift
@@ -0,0 +1,39 @@
+//Given a linked list, swap every two adjacent nodes and return its head.
+//
+//You may not modify the values in the list's nodes, only nodes itself may be changed.
+//
+// 
+//
+//Example:
+//
+//Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+import UIKit
+
+
+//* Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+
+class SolutionPairs {
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        
+        let temp = head?.val
+        head?.val = head?.next?.val ?? 0
+        head?.next?.val = temp ?? 0
+        
+        if head?.next?.next != nil
+        {
+            swapPairs(head?.next?.next)
+        }
+            
+        return head
+    }
+}
