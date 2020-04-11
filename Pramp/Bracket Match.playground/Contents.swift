dad9ee58eef8ceb148aef669527321da1b966be2
Ajinkya Shelar <ajinkya.shelar@gmail.com>
2020-04-11 10:40:17 +0530

    Bracket Match - Pramp - 1st Mock Question for other quy.

---
+++ Pramp/Bracket Match.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+//Bracket Match
+//A string of brackets is considered correctly matched if every opening bracket in the string can be paired up with a later closing bracket, and vice versa. For instance, “(())()” is correctly matched, whereas “)(“ and “((” aren’t. For instance, “((” could become correctly matched by adding two closing brackets at the end, so you’d return 2.
+//
+//Given a string that consists of brackets, write a function bracketMatch that takes a bracket string as an input and returns the minimum number of brackets you’d need to add to the input in order to make it correctly matched.
+//
+//Explain the correctness of your code, and analyze its time and space complexities.
+//
+//Examples:
+//
+//input:  text = “(()”
+//output: 1
+//
+//input:  text = “(())”
+//output: 0
+//
+//input:  text = “())(”
+//output: 2
+//Constraints:
+//
+//[time limit] 5000ms
+//
+//[input] string text
+//
+//1 ≤ text.length ≤ 5000
+//[output] integer
+
+class Solution {
+    func bracketMatch(text: String) -> Int {
+        let array : [String] = text.map({String($0)})
+        var bracketArray : [String] = []
+        for i in array {
+          if i == "(" {
+            bracketArray.append(i)
+          }
+          else if i == ")" && bracketArray.last == "(" {
+            bracketArray.removeLast()
+          }
+          else  {
+            bracketArray.append(i)
+          }
+        }
+        return bracketArray.count
+    }
+}
+
+let sol : Solution = Solution()
+print(sol.bracketMatch(text: "()))"))
