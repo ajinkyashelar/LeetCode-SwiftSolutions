76c7a3388abe98358704b73431bb3b784dde33fe
Ajinkya Shelar <ajinkya.shelar@gmail.com>
2020-08-01 20:33:46 +0530

    D1-Detect Capital

---
+++ Sources/LeetCode-SwiftSolutions/August LeetCoding Challenge/D1-Detect Capital.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+//Detect Capital
+//Given a word, you need to judge whether the usage of capitals in it is right or not.
+//
+//We define the usage of capitals in a word to be right when one of the following cases holds:
+//
+//All letters in this word are capitals, like "USA".
+//All letters in this word are not capitals, like "leetcode".
+//Only the first letter in this word is capital, like "Google".
+//Otherwise, we define that this word doesn't use capitals in a right way.
+//
+//
+//Example 1:
+//
+//Input: "USA"
+//Output: True
+//
+//
+//Example 2:
+//
+//Input: "FlaG"
+//Output: False
+//
+//
+//Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+
+class Solution {
+    func detectCapitalUse(_ word: String) -> Bool {
+        if word == word.uppercased() {
+            return true
+        }
+        else if word == word.capitalized {
+            return true
+        }
+        else if word == word.lowercased() {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
